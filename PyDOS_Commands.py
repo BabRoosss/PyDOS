23c66edf6a51535bdba4df9357f2988c9614b4b8
BabRoosss <92652937+BabRoosss@users.noreply.github.com>
2022-02-01 21:53:22 -0500

    Add files via upload

---
+++ PyDOS_Commands.py
@@ -0,0 +1,51 @@
+from os import system
+
+def clear():
+	system('clear')
+def nano():
+	system('nano')
+def ls():
+	system('ls')
+def pwd():
+	system('pwd')
+def cd():
+	system('cd')
+def cdDown():
+	system('cd ..')
+def updateCurl
+	system('curl')
+clear()
+
+print('=========================PyDOS=========================')
+while 1==1:
+	print()
+	command = input('>')
+
+	if command == 'clear':
+		print('=========================PyDOS=========================')
+		clear()
+	if command == 'logout':
+		clear()
+		print('You have been loged out of your session.')
+		print('Please log in to continue using PyDOS!')
+		exec(open('PyDOS_Login.py').read())
+	if command == 'textedit':
+		nano()
+	if command == 'sysinfo':
+		print('Writen in Python 3')
+		print('Made by Bab Roosss')
+	if command == 'exit':
+		break
+	if command == 'ls':
+		print()
+		ls()
+	if command == 'pwd':
+		pwd()
+	if command == 'help':
+		print('Help	Prints help info')
+		print('Clear	Clears the screen')
+		print('Nano	Opens a TextEditor')
+		print('ls	Prints the contents of the current directory')
+		print('pwd	Prints current working directory')
+		print('sysinfo	Prints info about the shell')
+		print('exit	. . . exit')
